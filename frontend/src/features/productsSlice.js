a4edd327d589cd1c3ccbc3592185b9e1e5410d9c
lus-terry <lucija.klanjac@gmail.com>
2024-05-15 14:36:40 +0200

    redux

---
+++ frontend/src/features/productsSlice.js
@@ -0,0 +1,14 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+    items: [],
+    status: null
+};
+
+const productsSlice = createSlice({
+    name: "products",
+    initialState,
+    reducers: {}
+});
+
+export default productsSlice.reducer;
