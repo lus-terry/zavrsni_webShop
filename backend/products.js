b0b2108cfb1673c1fb95a0afef35d5133c2a108a
lus-terry <lucija.klanjac@gmail.com>
2024-05-15 14:20:39 +0200

    products

---
+++ backend/products.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+        id: 1,
+        name: "malvazija",
+        desc: "jako fino bla bla",
+        price: 10,
+        image: "https://res.cloudinary.com/lus-terry/image/upload/v1715771420/InSylvis/products/malvazija_svjeza.jpg"
+    },
+    {
+        id: 2,
+        name: "rose",
+        desc: "jako fino bla bla",
+        price: 10,
+        image: "https://res.cloudinary.com/lus-terry/image/upload/v1715771418/InSylvis/products/rose.jpg"
+    },
+    {
+        id: 3,
+        name: "arbor",
+        desc: "jako fino bla bla",
+        price: 13,
+        image: "https://res.cloudinary.com/lus-terry/image/upload/v1715771419/InSylvis/products/malvazija_arbor.jpg"
+    },
+    {
+        id: 4,
+        name: "cuve",
+        desc: "jako fino bla bla",
+        price: 13,
+        image: "https://res.cloudinary.com/lus-terry/image/upload/v1715771416/InSylvis/products/cuve.jpg"
+    },
+];
+
+module.exports = products;
