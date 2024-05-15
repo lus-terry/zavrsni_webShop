d03499f6b8643155b9893ebb6f5de5019ac27480
lus-terry <lucija.klanjac@gmail.com>
2024-05-15 16:42:00 +0200

    fetch products api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,12 +7,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 
 import productsReducer, { productsFetch } from './features/productsSlice';
+import { productsApi } from './features/productsApi';
 
 
 const store = configureStore({
   reducer:{
     products: productsReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(productsApi.middleware),
+  
 });
 
 store.dispatch(productsFetch());
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,7 @@
+import { useGetAllProductsQuery } from "../features/productsApi";
+
 const  Home = () => {
+    const {data, error, isLoading} = useGetAllProductsQuery();
     return (  <h2>Home</h2>);
 }
  
